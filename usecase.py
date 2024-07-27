cc1ad6d8ea1c2bb0b155872464d74b3eb9ab3157
Thierry <thierry.nancy@cyanview.com>
2024-07-27 09:14:08 +0200

    Comments Usecase

--- usecase.py
+++ usecase.py
@@ -8,8 +8,9 @@ class Usecase:
         self.property = Properties()
         self.df = pd.DataFrame()
         return
+    # Setup Usecase dataframe from Pool dataframe
     def setup(self,pool_df = None):
-        # create a dictionnary from the Pool dataframe
+        # Create a dictionnary from the Pool dataframe
         def dataframe_to_dic(pool_df):
             paths_dict = {}
             if not pool_df.empty:
@@ -20,10 +21,12 @@ class Usecase:
                         variables.extend(pool_df.loc[camera_index].tolist())
                         paths_dict[new_index] = list(variables)
             return (paths_dict)
+        # Create a dataframe from dictionnary   
         def dic_to_dataframe(paths_dict,pool_df):
             df = pd.DataFrame.from_dict(paths_dict, orient = 'index', columns = pool_df.columns.values)
             df.index.name = 'Instance'     
             return(df)
+        # Suppress and add columns
         def columns():
             self.df.drop(columns=['SupportURL', 'ManufacturerURL','Remark','Selected','Message'], inplace=True)
             self.df['Camera_id'] = self.df.index
