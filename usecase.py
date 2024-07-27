82d5f4609a9429baa7da5b04a8aee649dab38867
Thierry <thierry.nancy@cyanview.com>
2024-07-27 09:06:03 +0200

    Refactor Usecase setup for readibility

--- usecase.py
+++ usecase.py
@@ -2,23 +2,28 @@ import pandas as pd
 from property import Properties
 from mermaid.graph import Graph
 
-# Built a dataframe wiht camera instances
+# Built a dataframe with one line per camera instance
 class Usecase:
     def __init__(self) -> None:
         self.property = Properties()
         self.df = pd.DataFrame()
         return
-    def setup(self,cameras_df=None):
-        def dico(cameras_df):
+    def setup(self,pool_df = None):
+        # create a dictionnary from the Pool dataframe
+        def dataframe_to_dic(pool_df):
             paths_dict = {}
-            if not cameras_df.empty:
-                for camera_index in cameras_df.index.to_list():
-                    for i in range(int(cameras_df.loc[camera_index,'Number'])):
+            if not pool_df.empty:
+                for camera_index in pool_df.index.to_list():
+                    for i in range(int(pool_df.loc[camera_index,'Number'])):
                         new_index = str(camera_index) + "_" + str(i) 
                         variables = []
-                        variables.extend(cameras_df.loc[camera_index].tolist())
+                        variables.extend(pool_df.loc[camera_index].tolist())
                         paths_dict[new_index] = list(variables)
             return (paths_dict)
+        def dic_to_dataframe(paths_dict,pool_df):
+            df = pd.DataFrame.from_dict(paths_dict, orient = 'index', columns = pool_df.columns.values)
+            df.index.name = 'Instance'     
+            return(df)
         def columns():
             self.df.drop(columns=['SupportURL', 'ManufacturerURL','Remark','Selected','Message'], inplace=True)
             self.df['Camera_id'] = self.df.index
@@ -33,8 +38,7 @@ class Usecase:
             self.df['ControlNeeds']= "No needs"
             #self.df['lens_id']=""
             return
-        paths_dict = dico(cameras_df)
-        self.df.index.name = 'Instance'     
-        self.df = pd.DataFrame.from_dict(paths_dict, orient = 'index', columns = cameras_df.columns.values)
+        paths_dict = dataframe_to_dic(pool_df)
+        self.df = dic_to_dataframe(paths_dict,pool_df)
         columns()       
         return 
